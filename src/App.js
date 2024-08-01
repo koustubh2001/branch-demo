d3027c969452fe8c556409717a5ba255aa9dc66b
koustubh2001 <koustubhrayamane2001@gmail.com>
2024-08-01 13:04:38 +0530

    commit 2 - added attendence and live Classes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Attendence from "./Attendence";
+import Classes from "./Classes";
+
  
 function App() {
   return (
     <>
     <h1>KodNest App</h1>
+    <Attendence/>
+    <Classes/>
   
     </>
   );
